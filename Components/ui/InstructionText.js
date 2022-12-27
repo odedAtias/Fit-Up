aeec64b96b823fd442f9b8ff036187e26a847fb3
Oded <odedatias8115@gmail.com>
2022-12-27 03:49:06 +0200

    Starting working on the login page (Adding userName input and password)

---
+++ Components/ui/InstructionText.js
@@ -0,0 +1,21 @@
+//  RN Core components
+import { View, Text, StyleSheet } from 'react-native';
+
+//  InstructionsText component
+const InstructionText = ({ text }) => {
+	return (
+		<View>
+			<Text style={styles.text}>{text}</Text>
+		</View>
+	);
+};
+
+//	InstructionText StyleSheet
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 18,
+		marginBottom: 10,
+	},
+});
+
+export default InstructionText;
--- Screens/Login.js
+++ Screens/Login.js
@@ -1,22 +1,54 @@
 //  RN Core components & API imports
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, TextInput, Dimensions } from 'react-native';
+import InstructionText from '../Components/ui/InstructionText';
 //  Custom components imports
 import Logo from '../Components/ui/Logo';
 
 //  Login component
 const Login = () => {
 	return (
+		//  Login root container
 		<View style={styles.loginContainer}>
-			<Logo />
+			{/* Logo */}
+			<View>
+				<Logo />
+			</View>
+			{/* Sign in form */}
+			<View style={styles.inputContainer}>
+				<InstructionText text={'Username: '} />
+				<TextInput
+					style={styles.textInput}
+					placeholder='Enter your username'
+					placeholderTextColor={'#a6a6a6'}
+				/>
+			</View>
+			<View style={styles.inputContainer}>
+				<InstructionText text={'Password: '} />
+				<TextInput
+					style={styles.textInput}
+					placeholder='Enter your password'
+					placeholderTextColor={'#a6a6a6'}
+				/>
+			</View>
 		</View>
 	);
 };
+
+const screenDevice = Dimensions.get('window');
 
 //  Login StyleSheet
 const styles = StyleSheet.create({
 	loginContainer: {
 		flex: 1,
 		alignItems: 'center',
+	},
+	textInput: {
+		borderBottomColor: 'black',
+		borderBottomWidth: 1,
+	},
+	inputContainer: {
+		width: screenDevice.width < 400 ? '75%' : '85%',
+		marginBottom: '10%',
 	},
 });
 
