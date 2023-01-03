a74c43f663a230077c4f2e849e962948733e7fb2
Oded <odedatias8115@gmail.com>
2023-01-03 06:58:29 +0200

    Adding documentation and choosing a main font.

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+//	Hooks imports
+import { useFonts } from 'expo-font';
+//  RN Core components & API imports
+import { StyleSheet } from 'react-native';
 //  Custom components imports
 import Login from './Screens/Login';
 
+//	App component
 export default function App() {
+	//	fonts loader
+	const [fontsLoaded] = useFonts({
+		Lato: require('./Assets/Fonts/Lato-Regular.ttf'),
+	});
+
 	return <Login />;
 }
 
+//	App StyleSheet
 const styles = StyleSheet.create({});
